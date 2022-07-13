769631d4a4eecee261df0064140c16652a655a90
LisaTLe <lisa.le1127@gmail.com>
2022-07-12 22:22:00 -0600

    edit index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const fs = require("fs");
 
 // "uuid" package for unique id
-// const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const router = Router();
 
@@ -15,6 +15,7 @@ router.get("/", (req, res) => {
       res.status(404).send("Error!");
     } else {
       console.log("GET NOTES requested", JSON.parse(data));
+      //send response
       res.json(JSON.parse(data));
     }
   });
@@ -25,8 +26,22 @@ router.post("/", (req, res) => {
   let newNote = req.body;
 
   console.log("New note created:", newNote);
-  notes.push(newNote);
+  data.push({ newNote, id: uuidv4() });
+  fs.writeFile("./db/db.json", JSON.stringify(data), "utf-8", (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("New note successfully added into 'db.json' file!");
+    }
+  });
+  //send response
   res.json(newNote);
 });
 
 module.exports = router;
+
+//API DELETE Request
+router.delete("/:id", (req, res) => {
+  let noteId = req.params.id;
+  console.log(id);
+});
