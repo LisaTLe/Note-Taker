2c2893cb33928da1a6f09237eda769c6fccad6d4
LisaTLe <lisa.le1127@gmail.com>
2022-07-14 20:52:14 -0600

    edit server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 //Middleware
 app.use(express.static("public"));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", api);
 // app.use("/", index);
