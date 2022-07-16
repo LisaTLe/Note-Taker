56666af55a7281a47479760a2ad5efa199cf90f8
LisaTLe <lisa.le1127@gmail.com>
2022-07-16 10:07:51 -0600

    route changes and formatting

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 const { Router } = require("express");
 const path = require("path");
-// const notesLink = require("./notes");
+const notesLink = require("./notes");
 
 const router = Router();
 
 //connecting notes.html
-// router.use("/notes", notesLink);
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
-});
+router.use("/notes", notesLink);
+// router.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/notes.html"));
+// });
 
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
-});
+// router.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const fs = require("fs");
-const db = require("../Develop/db/db.json");
+// const db = require("../db/db.json");
 
 // "uuid" package for unique id
 const { v4: uuidv4 } = require("uuid");
@@ -8,8 +8,8 @@ const { v4: uuidv4 } = require("uuid");
 const router = Router();
 
 //API GET Requests
-router.get("/notes", (req, res) => {
-  fs.readFile(db, "utf-8", (err, data) => {
+router.get("/", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
     // res.json(JSON.parse(data))
     if (err) {
       console.log(err);
@@ -23,34 +23,51 @@ router.get("/notes", (req, res) => {
 });
 
 //API POST Requests
-router.post("/notes", (req, res) => {
+router.post("/", (req, res) => {
   let newNote = req.body;
 
   console.log("New note created:", newNote);
-  data.push({ newNote, id: uuidv4() });
-  fs.writeFile(db, JSON.stringify(data), "utf-8", (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("New note successfully added into 'db.json' file!");
-    }
-  });
-  //send response
-  res.json(newNote);
+  fs.readFile(
+    "./db/db.json",
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(404).send("Error!");
+      } else {
+        const existData = JSON.parse(data);
+        existData.push({ newNote, id: uuidv4() });
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(existData),
+          "utf-8",
+          (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("New note successfully added into 'db.json' file!");
+            }
+          }
+        );
+      }
+    },
+    //send response
+    res.json(newNote)
+  );
 });
 
 //API DELETE Request
-router.delete("/notes/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   let noteId = req.params.id;
   console.log("DELETE REQUEST FOR: ${noteId}");
-  fs.readFile(db, "utf-8", (err, data) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
     if (err) {
       console.log(err);
       res.send("There was an error for the DELETE request");
     } else {
       let oldNotes = JSON.parse(data);
       let newNotes = oldNotes.filter((note) => note.id !== noteId);
-      fs.writeFile(db, JSON.stringify(newNotes));
+      fs.writeFile("./db/db.json", JSON.stringify(newNotes));
       console.log("Note was successfully deleted!");
       //send response
       res.json(newNotes);
